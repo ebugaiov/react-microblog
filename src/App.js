3d5d002d0b4b3ac68ad2db8608eef908cfd2f849
EvgeniyBugaiov <evgeniy.bugaiov@gmail.com>
2024-09-03 14:00:39 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Header from './components/Header';
+import Body from './components/Body';
+import Posts from './components/Posts';
 
-function App() {
+export default function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="App">
+      <Header />
+      <Container>
+        <Body sidebar>
+          <Posts />
+        </Body>
+      </Container>
+    </Container>
   );
 }
-
-export default App;
